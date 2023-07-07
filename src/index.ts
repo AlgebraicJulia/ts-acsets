d8e3c8e8b277d6798c70e52ab2e5ed21557b9203
Owen Lynch <root@owenlynch.org>
2023-07-07 16:25:47 -0700

    debug ci

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export {ACSet, ExportedACSet} from "./acset";
-export {Ob, Hom, AttrType, Attr, CatlabSchema, Schema} from "./schema";
-export * as graph from "./graph";
+export { ACSet, ExportedACSet } from './acset';
+export { Ob, Hom, AttrType, Attr, CatlabSchema, Schema } from './schema';
+export * as graph from './graph';
